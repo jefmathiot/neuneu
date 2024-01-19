50f7c70dac3d93c7505f0a9d4efc59ec46151c9d
Jef Mathiot <jeff.mathiot@gmail.com>
2024-01-19 17:09:45 +0100

    Report coverage using simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+
+require "simplecov"
+SimpleCov.start
+
 require "neuronface"
 
 require "minitest/autorun"
